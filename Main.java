86c63cfb8351b33c5f7c75456e66c8bf7811c0f2
DedMazaev <55291929+DedMazaev@users.noreply.github.com>
2019-09-21 10:11:45 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,37 +11,44 @@ public class Main {
         System.out.println("2.Random");
         Scanner in = new Scanner(System.in);
         byte x=in.nextByte();
-
-
-   LabArr one=new LabArr();
-        one.procedure(x);
-
-    }
-}
-class LabArr {
-    public byte[] arr = new byte[5];
-    public void procedure(byte a) {
-        a = a;
-        Scanner in = new Scanner(System.in);
-        if (a == 1) {
+        System.out.println("Enter array size");
+        byte n=in.nextByte();
+        byte[] arr = new byte[n];
+        if (x == 1) {
             System.out.println("Enter array");
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < n; i++) {
                 arr[i] = in.nextByte();
             }
         } else {
             new Random().nextBytes(arr);
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < n; i++) {
 
                 System.out.println(arr[i]);
 
             }
 
         }
-        System.out.println("Enter x");
-        byte x = in.nextByte();
+
+
+        System.out.println("Enter digit to find elements of array with number of digits < than this digit");
+
+        byte u=in.nextByte();
+
+
+
+   LabArr one=new LabArr();
+        one.task1(u, arr, n);
+        one.task2(arr,n);
+
+    }
+}
+class LabArr {
+
+    public void task1(byte a, byte arr[], byte n) {
+         byte x=a;
         byte[] arr1 = arr;
         byte sum1 = 0;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < n; i++) {
             byte sum = 0;
             while (arr1[i] != 0) {
                 sum++;
@@ -53,10 +60,18 @@ class LabArr {
                 sum1++;
         }
         System.out.println("Amount of numbers with amount of digits <" + x + "=" + sum1);
-        if ((arr[0] == arr[4]) && (arr[1] == arr[3]))
-            System.out.println("Array is symmetrical");
+
+
+
+    }
+    public void task2(byte arr[], byte n){
+        int flag=0;
+        for (int i=0,j=n-1; i<n/2; i++,j--) {
+            if (arr[i]!=arr[j])
+                flag++;
+        }
+        if (flag==0)
+        System.out.println("Array is symmetrical");
         else System.out.println("Array is not symmetrical");
-
-
     }
 }
