1588e4dbc4e8725d3bee42dbee822fa350ca7381
DedMazaev <55291929+DedMazaev@users.noreply.github.com>
2019-12-05 01:50:37 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,167 +1,117 @@
 package com.company;
-import org.w3c.dom.Text;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-            int test = 8;
-            Scanner in = new Scanner(System.in);
-            while (test != 0) {
-                try {
-                    System.out.print("[0] - Завершение работы" + '\n' +
-                            "[1] - Вариант на String" + '\n' +
-                            "[2] - Ванриант на StringBuffer" + '\n' +
-                            "[3] - Вариант на регулярные выражения" + '\n' +
-                            "Выберите вариант: ");
-                    test = in.nextInt();
-                    switch (test) {
-                        case 0:
-                            System.out.print("End of working");
-                            break;
-                        case 1:
-                            testStringClass(in);
-                            break;
-                        case 2:
-                            testStringBuilderClass(in);
-                            break;
-                        case 3:
-                            testRegularExpressionsClass(in);
-                            break;
-                        default:
-                            System.out.println("Вы ввели недопустимую конструкцию!");
-                            break;
-                    }
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-            }
 
-    }
-    static void testStringClass(Scanner in) throws Exception {
-        TextWork object = new TextWork();
+    public static void main(String[] args) {
+        DistanceExaminator de = new DistanceExaminator();
+        Scanner in = new Scanner(System.in);
         int task = 8;
-        while (task != 0){
-            System.out.print("[0] - Предыдущее меню" + '\n' +
-                    "[1] - Ввод строки" + '\n' +
-                    "[2] - Вывод строки" + '\n' +
-                    "[3] - Задание 1" + '\n' +
-                    "[4] - Задание 2" + '\n' +
-                    "[5] - Задание 3" + '\n' +
-                    "[6] - Задание 4" + '\n' +
-                    "Выберете задание: ");
-            task = in.nextInt();
-            switch(task){
+        String name, login, password, passwordRep;
+        while (task != 0) {
+        try {
+                if (de.getCurreantUser() == -2)
+                    System.out.print("[0] - завершение работы" + '\n' +
+                    "[1] - регистрация в системе" + '\n' +
+                    "[2] - вход в ситему" + '\n');
+                if (de.getCurreantUser() == -1)
+                    System.out.print(
+                            "[3] - выход из системы" + '\n' +
+                            "[4] - добавление вопроса" + '\n');
+                if (de.getCurreantUser() >= 0)
+                    System.out.print(
+                            "[3] - выход из системы" + '\n' +
+                            "[5] - сдача экзамена" + '\n' +
+                            "[6] - узнать оценку" + '\n' +
+                            "[7] - сменить пароль" + '\n');
+                System.out.print("Выберите нужный пункт меню: ");
+                task = in.nextInt();
+                if (de.getCurreantUser() == -2)
+                    switch (task) {
                 case 0:
-                    System.out.println("Возвращение к предыдущему меню...");
+                    System.out.print("Завершение работы");
                     break;
                 case 1:
-                    System.out.println("Введите текст: ");
                     in.nextLine();
-                    String str = in.nextLine();
-                    object.inputText(str);
-                    break;
+                    System.out.print("Введите свое имя: ");
+                    name = in.nextLine();
+                    System.out.print("Введите логин: ");
+                    login = in.nextLine();
+                    System.out.print("Введите пароль: ");
+                    password = in.nextLine();
+                    System.out.print("Повторите пароль: ");
+                    passwordRep = in.nextLine();
+                    de.addUser(name, login, password, passwordRep);
+                    System.out.println("Вы успешно зарегистрировались");
+                    continue;
                 case 2:
-                    System.out.println(object.getText());
-                    break;
-                case 3:
-                    object.paragraphSegmentation();
-                    System.out.println("Текст разделенный на параграфы:\n" + object.getText());
-                    break;
-                case 4:
-                    object.toUpperWords();
-                    System.out.println("Текст с прописными буквами:\n" + object.getText());
-                    break;
-                case 5:
-                    System.out.print("Введите номер абзаца: ");
-                    int i = in.nextInt();
-                    System.out.println("Количество слов в абзаце №" + i + " с содержанием \"ам\" равно " + object.findAmIn(i));
-                    break;
-                case 6:
-                    System.out.println("Новая строка с включенным количеством слов:\n" + object.wordNum());
-                    break;
+                    in.nextLine();
+                    System.out.print("Введите логин: ");
+                    login = in.nextLine();
+                    System.out.print("Введите пароль: ");
+                    password = in.nextLine();
+                    de.logining(login, password);
+                    System.out.println("Вы успешно вошли в систему");
+                    continue;
                 default:
-                    System.out.println("Вы ввели недопустимую конструкцию!");
-                    break;
+                    System.out.println("Вы ввели непредусмотренную комбинацию символов");
+                    continue;
+            }
+                if (de.getCurreantUser() == -1)
+                    switch (task) {
+                        case 3:
+                            de.exit();
+                            System.out.println("Вы успешно вышли");
+                            continue;
+                        case 4:
+                            in.nextLine();
+                            System.out.print("Введите вопрос: ");
+                            login = in.nextLine();
+                            System.out.print("Введите ответ на вопрос: ");
+                            password = in.nextLine();
+                            de.addQuestion(login, password);
+                            System.out.println("Вопрос успешно добавлен");
+                            continue;
+                        default:
+                            System.out.println("Вы ввели непредусмотренную комбинацию символов");
+                            continue;
+                    }
+                if (de.getCurreantUser() >= 0)
+                    switch (task){
+                        case 3:
+                            de.exit();
+                            System.out.println("Вы успешно вышли");
+                            continue;
+                        case 5:
+                            System.out.println("Экзамен начался");
+                            de.examination();
+                            System.out.println("Экзамен завершен");
+                            continue;
+                        case 6:
+                            System.out.println("Ваша оценка за экзамен: " + de.getMark());
+                            continue;
+                        case 7:
+                            in.nextLine();
+                            System.out.print("Введите логин: ");
+                            name = in.nextLine();
+                            System.out.print("Введите страрый пароль: ");
+                            login = in.nextLine();
+                            System.out.print("Введите новый пароль: ");
+                            password = in.nextLine();
+                            System.out.print("Повторите новый пароль: ");
+                            passwordRep = in.nextLine();
+                            de.changePassword(name, login, password, passwordRep);
+                            System.out.println("Вы успешно сменили пароль");
+                            continue;
+                        default:
+                            System.out.println("Вы ввели непредусмотренную комбинацию символов");
+                            continue;
+                    }
+            }
+        catch(Exception e){
+                System.out.println(e);
             }
         }
     }
-    static void testStringBuilderClass(Scanner in) {
-        BilderTewtWork object = new BilderTewtWork();
-        int task = 8;
-        while (task != 0){
-            System.out.print("[0] - Предыдущее меню" + '\n' +
-                    "[1] - Ввод строки" + '\n' +
-                    "[2] - Вывод строки" + '\n' +
-                    "[3] - Задание 1" + '\n' +
-                    "[4] - Задание 2" + '\n' +
-                    "[5] - Задание 3" + '\n' +
-                    "Выберете задание: ");
-            task = in.nextInt();
-            switch(task){
-                case 0:
-                    System.out.println("Возвращение к предыдущему меню...");
-                    break;
-                case 1:
-                    System.out.println("Введите текст: ");
-                    in.nextLine();
-                    String str = in.nextLine();
-                    object.inputText(new StringBuilder(str));
-                    break;
-                case 2:
-                    System.out.println(object.getText());
-                    break;
-                case 3:
-                    object.paragraphSegmentation();
-                    System.out.println("Текст разделенный на параграфы:\n" + object.getText());
-                    break;
-                case 4:
-                    object.toUpperWords();
-                    System.out.println("Текст с прописными буквами:\n" + object.getText());
-                    break;
-                case 5:
-                    //ривет мир. я иван люблю делать ам! ам ам ам 3 раза сказал ам всего 4? ам было в 3 абзаце, вот!
-                    object.paragraphNum();
-                    System.out.println("Текст с добавленным предложением:\n" + object.getText());
-                    break;
-                default:
-                    System.out.println("Вы ввели недопустимую конструкцию!");
-                    break;
-            }
-        }
-    }
-    static void testRegularExpressionsClass(Scanner in) {
-        RegularExpression object = new RegularExpression();
-        int task = 8;
-        while (task != 0){
-            System.out.print("[0] - Предыдущее меню" + '\n' +
-                    "[1] - Ввод строки" + '\n' +
-                    "[2] - Вывод строки" + '\n' +
-                    "[3] - Задание 1" + '\n' +
-                    "Выберете задание: ");
-            task = in.nextInt();
-            switch (task){
-                case 0:
-                    System.out.println("Возвращение к предыдущему меню...");
-                    break;
-                case 1:
-                    System.out.println("Введите текст: ");
-                    in.nextLine();
-                    String str = in.nextLine();
-                    object.inputText(str);
-                    break;
-                case 2:
-                    System.out.println("Ваш текст:\n" + object.ghetText());
-                    break;
-                case 3:
-                    object.changeNum();
-                    System.out.println("Измененный текст:\n" + object.ghetText());
-                    break;
-                default:
-                    System.out.println("Вы ввели недопустимую конструкцию!");
-                    break;
-            }
-        }
-    }
-} 
+}
